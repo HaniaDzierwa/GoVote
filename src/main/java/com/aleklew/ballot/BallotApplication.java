962817403b26560d4b1800b4681ac25786865049
Aleksandra <ola.lewandowska12@gmail.com>
2022-12-07 17:15:29 +0100

    swagger

--- src/main/java/com/aleklew/ballot/BallotApplication.java
+++ src/main/java/com/aleklew/ballot/BallotApplication.java
@@ -3,9 +3,9 @@ package com.aleklew.ballot;
 import org.hibernate.cfg.DefaultNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.aleklew.ballot"})
 @SpringBootApplication
 public class BallotApplication {
 
