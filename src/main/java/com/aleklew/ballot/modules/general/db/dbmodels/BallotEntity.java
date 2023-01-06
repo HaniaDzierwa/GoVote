e5dfb4cc26b50243a360fb606732a505cf6954b0
Hanna Dzierwa <hanndzi314@student.polsl.pl>
2023-01-06 15:54:38 +0100

    quick fix

--- src/main/java/com/aleklew/ballot/modules/general/db/dbmodels/BallotEntity.java
+++ src/main/java/com/aleklew/ballot/modules/general/db/dbmodels/BallotEntity.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
